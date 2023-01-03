e33c3958b9d6aa1db3a09a9a5461e2cf75ef7071
tahj11 <jtahj25@gmail.com>
2023-01-03 17:53:28 -0500

    Removed Login and Registration folders

--- client/library/src/App.js
+++ client/library/src/App.js
@@ -1,17 +1,12 @@
 import "./App.css";
 import Home from "./pages/home/Home";
-import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Home />
-      {/* <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes> */}
+      {/* <Home /> */}
+      <Login />
     </div>
   );
 }
--- client/library/src/pages/login/Login.jsx
+++ client/library/src/pages/login/Login.jsx
@@ -1,49 +1,8 @@
-import { useState } from "react";
+import React from "react";
 import "./login.css";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  async function loginUser(event) {
-    event.preventDefault();
-    const response = await fetch("http://localhost:1337/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    });
-
-    const data = await response.json();
-
-    console.log(data);
-  }
-  return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={loginUser}>
-        <input
-          value={email}
-          type="text"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <input
-          value={password}
-          type="text"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  );
+  return <div>Login</div>;
 }
 
 export default Login;
